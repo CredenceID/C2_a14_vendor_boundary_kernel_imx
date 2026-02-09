boundarydevices/linux

ov5640_mipi_v2: improve reset function
Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

drivers/media/platform/mxc/capture/ov5640_mipi_v2.c
@@ -1422,21 +1422,30 @@ static void ov5640_reset(struct ov5640 *sensor)
 	if (sensor->rst_gpio < 0 || sensor->pwn_gpio < 0)
 		return;
 
-	/* camera reset */
-	gpio_set_value(sensor->rst_gpio, 1);
+	gpio_set_value(sensor->rst_gpio, 0);	/* camera reset */
+	gpio_set_value(sensor->pwn_gpio, 1);	/* camera power down */
 
-	/* camera power dowmn */
-	gpio_set_value(sensor->pwn_gpio, 1);
+	/* >= 5 ms, Let power supply stabilize */
 	msleep(5);
-
-	gpio_set_value(sensor->rst_gpio, 0);
-	msleep(1);
+	mutex_lock(&ov5640_mutex);
 
 	gpio_set_value(sensor->pwn_gpio, 0);
-	msleep(5);
 
+	/* >= 1ms from powerup, to reset release*/
+	msleep(1);
 	gpio_set_value(sensor->rst_gpio, 1);
-	msleep(5);
+
+	/* >= 20 ms from reset high to SCCB initialized */
+	msleep(20);
+	pr_debug("%s(mipi): reset released\n", __func__);
+	ov5640_update_slave_id(sensor);
+	mutex_unlock(&ov5640_mutex);
+}
+
+static void ov5640_reset_pwrdn(struct ov5640 *sensor)
+{
+	ov5640_reset(sensor);
+	gpio_set_value(sensor->pwn_gpio, 1);
 }
 
 static int power_up(struct ov5640 *sensor)
@@ -1477,6 +1486,8 @@ static int power_up(struct ov5640 *sensor)
 			goto err4;
 		}
 	}
+	/* Make sure power is on */
+	ov5640_power_down(sensor, 0);
 	sensor->on = 1;
 	return 0;
 err4:
@@ -2902,9 +2913,7 @@ static int ov5640_s_parm(struct v4l2_subdev *sd, struct v4l2_streamparm *a)
 	enum ov5640_mode orig_mode;
 	int ret = 0;
 
-	/* Make sure power on */
-	if (gpio_get_value(sensor->pwn_gpio))
-		ov5640_power_down(sensor, 0);
+	power_up(sensor);	/* Make sure power is on */
 
 	switch (a->type) {
 	/* This is the only case currently handled. */
@@ -3564,7 +3573,7 @@ static int ov5640_probe(struct i2c_client *client,
 		return -EINVAL;
 	}
 
-	sensor->io_init = ov5640_reset;
+	sensor->io_init = ov5640_reset_pwrdn;
 	sensor->i2c_client = client;
 	sensor->pix.pixelformat = V4L2_PIX_FMT_YUYV;
 	sensor->pix.width = 640;
@@ -3577,17 +3586,7 @@ static int ov5640_probe(struct i2c_client *client,
 
 	ov5640_regulator_enable(sensor);
 
-	mutex_lock(&ov5640_mutex);
-	{
-		ov5640_reset(sensor);
-		ov5640_power_down(sensor, 0);
-		retval = ov5640_update_slave_id(sensor);
-	}
-	mutex_unlock(&ov5640_mutex);
-	if (retval < 0) {
-		clk_disable_unprepare(sensor->sensor_clk);
-		return -ENODEV;
-	}
+	ov5640_reset(sensor);
 
 	retval = ov5640_read_reg(sensor, OV5640_CHIP_ID_HIGH_BYTE,
 				 &chip_id_high);
@@ -3612,7 +3611,7 @@ static int ov5640_probe(struct i2c_client *client,
 	if (retval < 0) {
 		clk_disable_unprepare(sensor->sensor_clk);
 		dev_warn(dev, "Camera init failed\n");
-		ov5640_power_down(sensor, 1);
+		power_down(sensor);
 		return retval;
 	}
 

CredenceID/C2_a14_vendor_boundary_kernel_imx

drivers/media/platform/mxc/capture/ov5640_mipi_v2.c
@@ -1433,21 +1433,30 @@ static void ov5640_reset(struct ov5640 *sensor)
 	if (sensor->rst_gpio < 0 || sensor->pwn_gpio < 0)
 		return;
 
-	/* camera reset */
-	gpio_set_value(sensor->rst_gpio, 1);
+	gpio_set_value(sensor->rst_gpio, 0);	/* camera reset */
+	gpio_set_value(sensor->pwn_gpio, 1);	/* camera power down */
 
-	/* camera power dowmn */
-	gpio_set_value(sensor->pwn_gpio, 1);
+	/* >= 5 ms, Let power supply stabilize */
 	msleep(5);
-
-	gpio_set_value(sensor->rst_gpio, 0);
-	msleep(1);
+	mutex_lock(&ov5640_mutex);
 
 	gpio_set_value(sensor->pwn_gpio, 0);
-	msleep(5);
 
+	/* >= 1ms from powerup, to reset release*/
+	msleep(1);
 	gpio_set_value(sensor->rst_gpio, 1);
-	msleep(5);
+
+	/* >= 20 ms from reset high to SCCB initialized */
+	msleep(20);
+	pr_debug("%s(mipi): reset released\n", __func__);
+	ov5640_update_slave_id(sensor);
+	mutex_unlock(&ov5640_mutex);
+}
+
+static void ov5640_reset_pwrdn(struct ov5640 *sensor)
+{
+	ov5640_reset(sensor);
+	gpio_set_value(sensor->pwn_gpio, 1);
 }
 
 static int ov5640_power_on(struct ov5640 *sensor)
@@ -1488,6 +1497,8 @@ static int ov5640_power_on(struct ov5640 *sensor)
 			goto err4;
 		}
 	}
+	/* Make sure power is on */
+	ov5640_power_down(sensor, 0);
 	sensor->on = 1;
 	return 0;
 err4:
@@ -2936,9 +2947,7 @@ static int ov5640_s_parm(struct v4l2_subdev *sd, struct v4l2_streamparm *a)
 	enum ov5640_mode orig_mode;
 	int ret = 0;
 
-	/* Make sure power on */
-	if (gpio_get_value(sensor->pwn_gpio))
-		ov5640_power_down(sensor, 0);
+	ov5640_power_on(sensor);	/* Make sure power is on */
 
 	switch (a->type) {
 	/* This is the only case currently handled. */
@@ -3599,7 +3608,7 @@ static int ov5640_probe(struct i2c_client *client,
 		return -EINVAL;
 	}
 
-	sensor->io_init = ov5640_reset;
+	sensor->io_init = ov5640_reset_pwrdn;
 	sensor->i2c_client = client;
 	sensor->pix.pixelformat = V4L2_PIX_FMT_YUYV;
 	sensor->pix.width = 640;
@@ -3612,17 +3621,7 @@ static int ov5640_probe(struct i2c_client *client,
 
 	ov5640_regulator_enable(sensor);
 
-	mutex_lock(&ov5640_mutex);
-	{
-		ov5640_reset(sensor);
-		ov5640_power_down(sensor, 0);
-		retval = ov5640_update_slave_id(sensor);
-	}
-	mutex_unlock(&ov5640_mutex);
-	if (retval < 0) {
-		clk_disable_unprepare(sensor->sensor_clk);
-		return -ENODEV;
-	}
+	ov5640_reset(sensor);
 
 	retval = ov5640_read_reg(sensor, OV5640_CHIP_ID_HIGH_BYTE,
 				 &chip_id_high);
@@ -3650,7 +3649,7 @@ static int ov5640_probe(struct i2c_client *client,
 		ov5640_power_off(sensor);
 		clk_disable_unprepare(sensor->sensor_clk);
 		dev_warn(dev, "Camera init failed\n");
-		ov5640_power_down(sensor, 1);
+		ov5640_power_off(sensor);
 		return retval;
 	}
 